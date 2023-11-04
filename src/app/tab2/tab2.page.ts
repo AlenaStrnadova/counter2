17ba70f99b76931f8c542917950e74c47dc25245
AlenaStrnadova <strnadova@post.cz>
2023-11-04 13:02:39 +0100

    adding to the todo list layout and functionality

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -5,10 +5,30 @@ import { Component } from '@angular/core';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
+
+
 export class Tab2Page {
 
-  constructor() {}
+  taskName: any = ""; // Entered Text
+  taskList: any[] = [] ; // Array to store tasks
 
+  constructor() { }
+
+  //names: string[] = [];
+
+
+  addTask() {
+    if (this.taskName.length > 0) {
+      let task = this.taskName;
+      this.taskList.push(task);
+      this.taskName = "";
+    }
+  }
+
+
+
+  deleteTask(index: any) {
+    this.taskList.splice(index, 1);
+  }
 }
-
 
