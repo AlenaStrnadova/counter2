c16bfadcf775932fc40f7034f75f0f530ee64d9d
AlenaStrnadova <strnadova@post.cz>
2023-11-05 09:04:38 +0100

    quote api initial setup of the function

--- src/app/api/quote-api.service.ts
+++ src/app/api/quote-api.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class QuoteApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getTheQuote(): Observable<any> {
+    let url = ''
+    return this.http.get(url)
+  }
+
 }
