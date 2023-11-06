7880772025eac7012dc19829be52d94ddbac6cf8
AlenaStrnadova <strnadova@post.cz>
2023-11-06 15:34:56 +0100

    functionality to history in tab3 - buggy but works

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,23 +1,47 @@
 import { Component } from '@angular/core';
 import { QuoteApiService } from '../api/quote-api.service';
 import { Observable } from 'rxjs';
+import { HistoryServiceService } from '../storage/history-service.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
+
 export class Tab1Page {
 
   quoteOutput$: Observable<any> = this.apiService.getTheQuote()
+  historyArray: Array<string> = []
 
-  constructor(private apiService: QuoteApiService) {}
+  constructor(
+    private apiService: QuoteApiService,
+    private storage: HistoryServiceService
+    ) {}
 
-  quoteClicked() {
+    async ionViewDidEnter () {
+      const storedData = await this.storage.get('history');
+      if (storedData) {
+        this.historyArray = JSON.parse(storedData);
+      }
+    }
+  
+    quoteClicked() {
     this.quoteOutput$ = this.apiService.getTheQuote()
+
+    this.quoteOutput$.subscribe(data => {
+      console.log(data.content)
+      let historyItem = data.content
+      this.historyArray.unshift(historyItem)
+      this.storage.set('history', JSON.stringify(this.historyArray))
+    });      
+
+
     };
 
   }
 
 
-
+ 
+    
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { HistoryServiceService } from '../storage/history-service.service';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
+
+
 export class Tab3Page {
 
-  constructor() {}
+  storedData: Array<any> = []
+
+  constructor(private storage: HistoryServiceService) { }
+
+  async ionViewWillEnter() {
+    const storedData = await this.storage.get('history');
+    if (storedData) {
+      this.storedData = JSON.parse(storedData);
+      console.log(this.storedData)
+    }
+  }
 
 }
+
